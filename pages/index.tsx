ddbd77fa2f2270d1e6ea871bf0d150bcb3995d10
starhn87 <starhn87@gmail.com>
2022-03-07 17:16:52 +0900

    Feat: 버튼 컴포넌트 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import type { NextPage } from "next";
+import type { NextPage } from 'next'
 
-import styled from "styled-components";
+import styled from 'styled-components'
 
 const Home: NextPage = () => {
-  return <Main>nextjs</Main>;
-};
+  return <Main>nextjs</Main>
+}
 
 const Main = styled.main`
   min-width: 50rem;
@@ -12,6 +12,6 @@ const Main = styled.main`
   height: 100vh;
   border: solid 1px rgba(0, 0, 0, 0.2);
   margin: 0 auto;
-`;
+`
 
-export default Home;
+export default Home
